f3acf5ade58e56c67ee17a825deabb00871f67bd
chelsey0527 <chelsey0527@gmail.com>
2022-10-20 23:09:32 +0800

    add folder

--- week-4/app.py
+++ week-4/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, make_response, render_template, request, session, redirect, url_for
 app = Flask(
     __name__,
     static_folder="public", # 靜態檔案的資料夾名稱
@@ -15,43 +15,61 @@ def index():
 # ---------- 登入頁面 ----------
 
 # 1. flask sessions 版本
-@app.route("/signin", methods=["POST"])
+# @app.route("/signin", methods=["POST"])
+# def login():
+#     # POST query String **機密資訊必用**
+#     userName = request.form["userName"]
+#     password = request.form["password"]
+#     session["userName"] = userName
+#     session["password"] = password
+#     session["boologin"] = False
+#     if userName == "" or password == "":
+#         # 只要是空的就直接跳 error
+#         message = request.args.get("error", "請輸入帳號、密碼")
+#         return redirect(url_for("error", message = message))
+#     else:
+#         # 非空，檢查帳號密碼是否為 test
+#         if userName != "test" or password != "test":
+#             message = request.args.get("error", "帳號、密碼輸入錯誤")
+#             return redirect(url_for("error", message = message))
+#         else:
+#             session["boologin"] = True
+#             return redirect(url_for("member"))
+
+# 2. 直接操作 cookie 版本
+@app.route("/signin", methods=["POST", "GET"])
 def login():
     # POST query String **機密資訊必用**
-    userName = request.form["userName"]
-    password = request.form["password"]
-    session["userName"] = userName
-    session["password"] = password
-    session["boologin"] = False
+    name = str(request.form["userName"])
+    pw = str(request.form["password"])
+    # set cookie 中儲存的資料
+    resp = make_response(redirect(url_for("member")))
+    resp.set_cookie("userName", name)
+    resp.set_cookie("password", pw)
+    resp.set_cookie("userStatus", False)
+    # get cookie 中儲存的資料
+    userName = request.cookies.get('userName')
+    password = request.cookies.get('passwrod')
+    userStatus = request.cookies.get('userStatus')
+    print("------------------",userName,password, userStatus)
+    # 條件判斷
     if userName == "" or password == "":
         # 只要是空的就直接跳 error
+        userStatus = False
         message = request.args.get("error", "請輸入帳號、密碼")
-        return redirect(url_for("error", message = message))
+        resp = make_response(redirect(url_for("error", message = message)))
+        return resp
     else:
         # 非空，檢查帳號密碼是否為 test
         if userName != "test" or password != "test":
+            userStatus = False
             message = request.args.get("error", "帳號、密碼輸入錯誤")
-            return redirect(url_for("error", message = message))
+            resp = make_response(redirect(url_for("error", message = message)))
+            return resp
         else:
-            session["boologin"] = True
-            return redirect(url_for("member"))
-
-# 2. 直接操作 cookie 版本
-# set cookie
-# @app.route('/setcookie', methods = ['POST', 'GET'])
-# def setcookie():
-#    if request.method == 'POST':
-#    user = request.form['nm']
-   
-#    resp = make_response(render_template('readcookie.html'))
-#    resp.set_cookie('userID', user)
-   
-#    return resp
-# get cookie
-# @app.route('/getcookie')
-# def getcookie():
-#    name = request.cookies.get('userID')
-#    return '<h1>welcome ' + name + '</h1>'
+            userStatus = True
+            # redirect 到 member 頁面
+            return resp
 
 
 # ---------- 錯誤頁面 ----------
@@ -64,7 +82,9 @@ def error():
 # ---------- 登入，進到 member 頁面 ----------
 @app.route("/member")
 def member():
-    if session["boologin"] == True:
+    # get cookie 中儲存的資料
+    userStatus = request.cookies.get('userStatus')
+    if userStatus == True:
         return render_template("member.html")
     else:
         return redirect(url_for("index"))
@@ -74,9 +94,11 @@ def member():
 @app.route("/signout")
 def signout():
     # 登出後將 session 帳密移除，並且是否登入顯示為 False
-    session.pop("username", None)
-    session.pop("password", None)
-    session["boologin"] = False
+    # session.pop("username", None)
+    # session.pop("password", None)
+    # session["boologin"] = False
+    resp.delete_cookie('userName')
+    resp.delete_cookie('password')
     return redirect(url_for("index"))
 
 # ---------- dynamic routing 格式設定，讓 js call 頁面時可以成功 ----------
