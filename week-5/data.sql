ea1ab7afe8b04705f99163148e213443bbbab225
李沁珊 <49125778+chelsey0527@users.noreply.github.com>
2022-10-20 22:37:40 +0800

    finish hw1 to hw4

--- week-5/data.sql
+++ week-5/data.sql
@@ -1,4 +1,4 @@
------ HW2 -----
+---------- HW2 ----------
 CREATE TABLE member(
 	id bigint AUTO_INCREMENT,
 	name VARCHAR(255) NOT NULL ,
@@ -7,5 +7,39 @@ CREATE TABLE member(
 	follower_count int unsigned NOT NULL DEFAULT 0,
 	time datetime NOT NULL DEFAULT current_timestamp(),
 	PRIMARY KEY (id) 
-)
-;
+);
+
+
+---------- HW3 ----------
+--- 1 ---
+INSERT INTO member (name, username, password, follower_count, time)
+VALUES ('mem1', 'test', 'test', 1, current_timestamp());
+--- 2 ---
+SELECT * FROM member;
+--- 3 ---
+SELECT * FROM member ORDER BY time DESC;
+--- 4 ---
+SELECT * FROM member WHERE id>1 AND id<5 ORDER BY time DESC;
+--- 5 ---
+SELECT * FROM member WHERE username = "test";
+--- 6 ---
+SELECT * FROM member WHERE username = "test" AND password = "test";
+--- 7 ---
+UPDATE member
+SET name = "test2"
+WHERE username = "test";
+
+
+---------- HW4 ----------
+--- 1 ---
+SELECT COUNT(id) FROM member;
+--- 2 ---
+SELECT SUM(follower_count) FROM member;
+--- 3 ---
+SELECT AVG(follower_count) FROM member;
+
+
+
+
+
+
